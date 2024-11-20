bd9a82de0fb8ac947a5d0f117c77bc73ffbe0564
corazon03 <yleyva@itparral.edu.mx>
2024-11-19 23:59:36 -0600

    Responsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,9 +13,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  items = [ { title: 'Card 1', description: 'Descripción de la card 1', image: 'assets/image1.jpg' },
-            { title: 'Card 2', description: 'Descripción de la card 2', image: 'assets/image2.jpg' },
-            { title: 'Card 3', description: 'Descripción de la card 3', image: 'assets/image3.jpg' } ];
+  items = [ { title: 'Romance', description: 'El género literario de romance es un tipo de literatura que se centra en relaciones amorosas como el tema principal. Su objetivo principal es explorar los sentimientos, emociones y desafíos de los personajes en el contexto de una historia de amor.', image: '/Romance.jpg' },
+            { title: 'Fantasía', description: 'El género de fantasía es un tipo de literatura que se caracteriza por incluir elementos mágicos, sobrenaturales o irreales, que no pueden explicarse por las leyes naturales. Estas historias suelen desarrollarse en mundos imaginarios o alternativos donde la magia, criaturas míticas y eventos extraordinarios son comunes.', image: '/Fantasia.jpg' },
+            { title: 'Thriller', description: 'El thriller es un género literario que se caracteriza por mantener al lector en un estado de suspenso, intriga y emoción constante. Estas historias suelen tener un ritmo rápido, giros inesperados y situaciones llenas de tensión que buscan atrapar la atención del lector desde el principio hasta el final.', image: '/Thriller.jpg' } ];
           
   selectedImage: string | null = null;
   
