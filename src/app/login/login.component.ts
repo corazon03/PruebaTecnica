66edcec13c6832d009e6f7dfc90a38c268e27826
DAvalos36 <davalos@itparral.edu.mx>
2024-11-20 23:00:58 -0600

    login con mensajes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MatTabsModule} from '@angular/material/tabs';
 import { MatCard } from '@angular/material/card';
 import { MatInput } from '@angular/material/input';
-import { MatFormField, MatLabel, MatError } from '@angular/material/form-field';
+import { MatFormField, MatLabel, MatError, MatHint } from '@angular/material/form-field';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LoginServiceService } from '../loginService/login-service.service';
 import { NgIf } from '@angular/common';
@@ -11,11 +11,14 @@ import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatTabsModule, MatFormField, MatLabel, MatError, ReactiveFormsModule, NgIf, MatInput, MatCard ],
+  imports: [MatTabsModule, MatFormField, MatLabel, MatError, ReactiveFormsModule, NgIf, MatInput, MatCard, MatHint ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  mensajeLogin = ""
+  mensajeRegistro = ""
+
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required)
@@ -34,14 +37,18 @@ export class LoginComponent {
     if (this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
       const result = this.loginService.login(email as string, password as string);
-      alert(result);
+      // alert(result);
+      this.mensajeLogin = result
     }
   }
   onRegister() {
     if (this.registerForm.valid) {
       const { nombre, email, password } = this.registerForm.value;
       const result = this.loginService.register(email as string, password as string, nombre as string);
-      alert(result);
+      if (result === 'Usuario registrado con éxito.'){
+        this.registerForm.reset()
+      }
+      this.mensajeRegistro = result
     }
   }
 
