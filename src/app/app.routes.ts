887ecd699122d29fdc46fc34c4f1e85b8339e03b
DAvalos36 <davalos@itparral.edu.mx>
2024-11-20 23:55:07 -0600

    ruta listar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './login/login.component'
 import {HomeComponent} from './home/home.component'
+import { ListadoComponent } from './listado/listado.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'login', component: LoginComponent }
+    {path: 'login', component: LoginComponent },
+    {path: 'libros', component: ListadoComponent}
 ];
