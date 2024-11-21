df9ccde9e6cee4fb2e0f19755c5ebe12682800b4
DAvalos36 <davalos@itparral.edu.mx>
2024-11-21 02:27:49 -0600

    listado termianod

--- src/app/listado/listado.component.ts
+++ src/app/listado/listado.component.ts
@@ -4,6 +4,25 @@ import { NgFor, NgIf } from '@angular/common';
 import { MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { MatButton } from '@angular/material/button';
+import { HttpClient } from '@angular/common/http';
+import { MatDialog } from '@angular/material/dialog';
+import { DetalleLibroComponent } from '../detalle-libro/detalle-libro.component';
+
+export interface DetailsResponse {
+  status: string
+  id: string
+  title: string
+  subtitle: string
+  description: string
+  authors: string
+  publisher: string
+  pages: string
+  year: string
+  image: string
+  url: string
+  download: string
+}
+
 
 @Component({
   selector: 'app-listado',
@@ -14,11 +33,17 @@ import { MatButton } from '@angular/material/button';
 })
 export class ListadoComponent {
 
- constructor(public listadoService: ListadoService) {
+ constructor(public listadoService: ListadoService, private http: HttpClient, private dialog: MatDialog) {
   afterNextRender(() => {
     this.listadoService.getBooks();
   })
  }
+
+  getDetails(id: string){
+    this.http.get<DetailsResponse>(`https://www.dbooks.org/api/book/${id}`).subscribe(det => {
+       this.dialog.open(DetalleLibroComponent, { data: det})
+    })
+  }
 
   onNextPage() {
     this.listadoService.nextPage();
